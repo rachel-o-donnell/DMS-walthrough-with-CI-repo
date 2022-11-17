4481ab3ec36efae3a0ff4835a88ebe11c1c14c85
rachel-o-donnell <hellotherachelo@gmail.com>
2022-11-17 13:36:14 +0000

    Fix bugs

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -47,8 +47,8 @@ def add_task():
     categories = list(Category.query.order_by(Category.category_name).all())
     if request.method == "POST":
         task = Task(
-            task_name=request.form.get("task-name"),
-            task_description=request.form.get("task-description"),
+            task_name=request.form.get("task_name"),
+            task_description=request.form.get("task_description"),
             is_urgent=bool(True if request.form.get("is_urgent") else False),
             due_date=request.form.get("due_date"),
             category_id=request.form.get("category_id")
--- taskmanager/static/script.js
+++ taskmanager/static/script.js
@@ -1,16 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let sidenav = document.querySelectorAll('.sidenav');
+document.addEventListener("DOMContentLoaded", function () {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 
     // datepicker initialization
-    let data = document.querySelectorAll('.datepicker');
+    let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
         format: "dd mmmm, yyyy",
         i18n: {
             done: "Select"
         }
     });
+
     // select initialization
-    let selects = document.querySelectorAll('select');
+    let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
 });
